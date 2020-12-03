3eb439274ba6d112556ce07ef71546e254f28704
victorsand <victor.sand@gmail.com>
2020-12-03 15:48:06 +0100

    remove redundant struct

--- day3/day3.go
+++ day3/day3.go
@@ -5,31 +5,19 @@ import (
 	"fmt"
 )
 
-type grid struct {
-	Rows []string
-}
-
-func buildGrid(inputLines []string) grid {
-	var areaGrid grid
-	for _, line := range inputLines {
-		areaGrid.Rows = append(areaGrid.Rows, line)
-	}
-	return areaGrid
-}
-
-func countSlopeTrees(g grid, xStep int, yStep int) int {
+func countSlopeTrees(g []string, xStep int, yStep int) int {
 	numTrees := 0
 	xPos, yPos := 0, 0
-	numRows := len(g.Rows)
+	numRows := len(g)
 	for {
-		currentRow := g.Rows[yPos]
+		currentRow := g[yPos]
 		rowWidth := len(currentRow)
 		xPos = (xPos + xStep) % rowWidth
 		yPos = yPos + yStep
 		if yPos >= numRows {
 			break
 		}
-		if g.Rows[yPos][xPos] == '#' {
+		if g[yPos][xPos] == '#' {
 			numTrees++
 		}
 	}
@@ -37,8 +25,7 @@ func countSlopeTrees(g grid, xStep int, yStep int) int {
 }
 
 func main() {
-	var inputLines = util.ReadFile("day3/input.txt")
-	areaGrid := buildGrid(inputLines)
+	var areaGrid = util.ReadFile("day3/input.txt")
 
 	// part 1
 	// 274
