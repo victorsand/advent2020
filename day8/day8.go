414ede804036742cdd93ab5baf1f05a71f582346
Victor Sand <victor.sand@gmail.com>
2020-12-08 13:13:05 +0100

    day 8 part 2

--- day8/day8.go
+++ day8/day8.go
@@ -28,6 +28,58 @@ func parseInstructions(lines []string) []instruction {
 	return instructions
 }
 
+func resetRunFlags(instructions []instruction) {
+	for i := range instructions {
+		instructions[i].hasRun = false
+	}
+}
+
+func switchOperation(instructions []instruction, index int) {
+	if instructions[index].operation == jump {
+		instructions[index].operation = noOperation
+	} else if instructions[index].operation == noOperation {
+		instructions[index].operation = jump
+	}
+}
+
+func switchAndRun(instructions []instruction) {
+	operationToSwitch := 0
+	isBroken := true
+	for operationToSwitch < len(instructions) && isBroken {
+		resetRunFlags(instructions)
+		switchOperation(instructions, operationToSwitch)
+		accumulator := int64(0)
+		next := 0
+		for next < len(instructions) {
+			current := next
+			instruction := instructions[next]
+			if instruction.hasRun {
+				isBroken = true
+				break
+			}
+			switch instruction.operation {
+			case accumulate:
+				accumulator += instruction.value
+				next++
+			case jump:
+				next += int(instruction.value)
+			case noOperation:
+				next++
+			default:
+				panic("Unknown operation" + instruction.operation)
+
+			}
+			instructions[current].hasRun = true
+		}
+		if next >= len(instructions) {
+			fmt.Println("FIXED! Accumulator value:", accumulator)
+			break
+		}
+		switchOperation(instructions, operationToSwitch)
+		operationToSwitch++
+	}
+}
+
 func main() {
 	inputLines := util.ReadFile("day8/input.txt")
 	instructions := parseInstructions(inputLines)
@@ -38,7 +90,7 @@ func main() {
 		current := next
 		instruction := instructions[next]
 		if instruction.hasRun {
-			fmt.Println("Loop detected! Accumulator value is ", accumulator)
+			fmt.Println("Loop detected!")
 			break
 		}
 		switch instruction.operation {
@@ -55,5 +107,8 @@ func main() {
 		}
 		instructions[current].hasRun = true
 	}
+	fmt.Println("Accumulator value:", accumulator)
+
+	switchAndRun(instructions)
 
 }
